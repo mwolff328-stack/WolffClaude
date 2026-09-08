e77e4948ab1202ff2a048717c52a3a3f411f2939
mwolff328-stack <mwolff328@gmail.com>
2026-09-08 06:44:00 -0700

    chore: auto-sync 2026-09-08 13:43 — session-env/23422c92-d4e1-4461-8c61-d986ed52b209/, session-env/b079221c-fcfa-4fa9-b4d5-48931332c728/

---
+++ session-env/23422c92-d4e1-4461-8c61-d986ed52b209/sessionstart-hook-3.sh
@@ -0,0 +1,3 @@
+export CODEX_COMPANION_SESSION_ID='23422c92-d4e1-4461-8c61-d986ed52b209'
+export CODEX_COMPANION_TRANSCRIPT_PATH='C:\Users\wolff\.claude\projects\C--Users-wolff-Projects-SurvivorPulse\23422c92-d4e1-4461-8c61-d986ed52b209.jsonl'
+export CLAUDE_PLUGIN_DATA='C:/Users/wolff/.claude/plugins/data/codex-openai-codex'
---
+++ session-env/b079221c-fcfa-4fa9-b4d5-48931332c728/sessionstart-hook-4.sh
@@ -0,0 +1,3 @@
+export CODEX_COMPANION_SESSION_ID='b079221c-fcfa-4fa9-b4d5-48931332c728'
+export CODEX_COMPANION_TRANSCRIPT_PATH='C:\Users\wolff\.claude\projects\C--Users-wolff-Projects-SurvivorPulse\b079221c-fcfa-4fa9-b4d5-48931332c728.jsonl'
+export CLAUDE_PLUGIN_DATA='C:/Users/wolff/.claude/plugins/data/codex-openai-codex'
