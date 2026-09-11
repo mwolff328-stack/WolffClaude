b19f04cbd76af1154af05cbd36b680b66776db28
mwolff328-stack <mwolff328@gmail.com>
2026-09-10 19:50:48 -0700

    chore: auto-sync 2026-09-11 02:50 — session-env/65bebbb4-1cb6-48c6-a6c8-a76a36a37231/

---
+++ session-env/65bebbb4-1cb6-48c6-a6c8-a76a36a37231/sessionstart-hook-4.sh
@@ -0,0 +1,3 @@
+export CODEX_COMPANION_SESSION_ID='65bebbb4-1cb6-48c6-a6c8-a76a36a37231'
+export CODEX_COMPANION_TRANSCRIPT_PATH='C:\Users\wolff\.claude\projects\C--Users-wolff\65bebbb4-1cb6-48c6-a6c8-a76a36a37231.jsonl'
+export CLAUDE_PLUGIN_DATA='C:/Users/wolff/.claude/plugins/data/codex-openai-codex'
